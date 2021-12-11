041c323c7f53396eec3927941433a2b9888c8fc4
Marco Antonio Carcano <me@mydomain.tld>
2021-12-11 16:31:11 +0000

    Improved setup a little bit more

--- src/setup.py
+++ src/setup.py
@@ -5,9 +5,28 @@ setup(
     version='0.0.1',
     author='Marco Antonio Carcano',
     author_email='myemailaddress@mydomain.tld',
+    description='An example list object that exploit Pyhon iteratable facilities',
+    classifiers=[
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+    ],
     url= 'https://github.com/mac-grimoire/python-spells.git',
+    license= 'GNU Lesser General Public License v3 or later',
+    keywords = 'iteratable list',
+    project_urls={
+        'Bug Tracker': 'https://github.com/mac-grimoire/python-spells.git',
+        'Documentation': 'https://github.com/mac-grimoire/python-spells.git',
+        'Source Code': 'https://github.com/mac-grimoire/python-spells.git',
+    },    
     packages=find_namespace_packages(include=['carcano.*']),
+    scripts=["bin/fooapp.py"],
+    data_files=[
+        ("bin", ["bin/logging.conf"]),
+        ("share/doc/fooapp/rsyslog", ["share/doc/fooapp/rsyslog/fooapp.conf"])
+    ],
     setup_requires=['nose>=1.0'],
     test_suite="nose.collector",
-    tests_require=["nose"],
+    tests_require=["nose"]
 )
